22938479e288289f66783c029ef53d27b09390b1
Ivan Flores <ifloresmanc96@gmail.com>
2025-11-25 00:14:16 -0300

    KMS-25 page header + detail side updates

--- src/app/modules/proveedores/components/proveedores-header/proveedores-header.ts
+++ src/app/modules/proveedores/components/proveedores-header/proveedores-header.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Button } from '../../../../shared/components/button/button';
+import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 
 @Component({
     selector: 'app-proveedores-header',
-    imports: [MatButtonModule, Button],
+    imports: [MatButtonModule, Button, PageHeaderComponent],
     templateUrl: './proveedores-header.html',
     styleUrl: './proveedores-header.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
