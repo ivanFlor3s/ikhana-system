8a4a4e15bab56911c15e4bab6062efc293866c57
Ivan Flores <ifloresmanc96@gmail.com>
2025-12-14 21:30:58 -0300

    rubros header

--- src/app/modules/rubros/rubros-header/rubros-header.component.ts
+++ src/app/modules/rubros/rubros-header/rubros-header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { PageHeaderComponent } from "@shared/components/page-header/page-header.component";
+import { Button } from "@shared/components/button/button";
+import { TagsIcon, LucideAngularModule } from "lucide-angular";
 
 @Component({
   selector: 'app-rubros-header',
-  imports: [],
+  imports: [PageHeaderComponent, Button, LucideAngularModule],
   templateUrl: './rubros-header.component.html',
   styleUrl: './rubros-header.component.css'
 })
 export class RubrosHeaderComponent {
+
+  readonly TagsIcon = TagsIcon;
+
 
 }
--- src/app/pages/rubros-page/rubros-page.component.ts
+++ src/app/pages/rubros-page/rubros-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RubrosHeaderComponent } from "@modules/rubros/rubros-header/rubros-header.component";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { RubrosListComponent } from "@modules/rubros/rubros-list/rubros-list.component";
 
 @Component({
   selector: 'app-rubros-page',
-  imports: [],
+  imports: [RubrosHeaderComponent, MatProgressSpinnerModule, RubrosListComponent],
   templateUrl: './rubros-page.component.html',
   styleUrl: './rubros-page.component.css'
 })
