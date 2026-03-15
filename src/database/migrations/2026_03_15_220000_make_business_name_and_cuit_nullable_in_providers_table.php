6ba42291df18bc15caad72d34ecb8e3d9b944a5b
iflores <iflores96@gmail.com>
2026-03-15 19:07:54 -0300

    feat: nullable fields cuit and business name

---
+++ src/database/migrations/2026_03_15_220000_make_business_name_and_cuit_nullable_in_providers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('providers', function (Blueprint $table) {
+            $table->string('business_name')->nullable()->change();
+            $table->string('cuit')->nullable()->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('providers', function (Blueprint $table) {
+            $table->string('business_name')->nullable(false)->change();
+            $table->string('cuit')->nullable(false)->unique()->change();
+        });
+    }
+};
