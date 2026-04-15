cf28a5c20a16ba34e4932d0d17c38a019a84bf08
iflores <iflores96@gmail.com>
2026-04-14 21:24:13 -0300

    add bobinas list empty component

---
+++ src/app/modules/proveedores/components/bobinas-list/bobinas-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bobinas-list',
+  imports: [],
+  templateUrl: './bobinas-list.component.html',
+  styleUrl: './bobinas-list.component.css'
+})
+export class BobinasListComponent {
+
+}
